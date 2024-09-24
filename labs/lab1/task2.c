6ccd5c8217bef3199ab1380f64dc94c09d7c624c
MMMMMMar22 <klever.vanlav@gmail.com>
2024-09-24 16:07:02 +0500

    Update task2.c

--- labs/lab1/task2.c
+++ labs/lab1/task2.c
@@ -3,16 +3,4 @@ int main() {
     int a = 3, b = 5, c = 0; 
     c = a + b; 
     printf("c= %d", c); //%d спецификатор формата int 
-}
-#include <stdio.h> 
-int main() { 
-    int a = 4, b = 7, c = 0; 
-    c = a + b; 
-    printf("c= %d", c); //%d спецификатор формата int 
-}
-#include <stdio.h> 
-int main() { 
-    int a = 10, b = 2, c = 0; 
-    c = a + b; 
-    printf("c= %d", c); //%d спецификатор формата int 
-}
+} // a = 4, b = 7, a = 10, b = 2
